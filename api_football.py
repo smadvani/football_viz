7f5c068f67a6871e148378b0040aeba55ef46a18
Sanjay Advani <sanjaymadvani@gmail.com>
2017-07-19 22:33:21 -0600

    add more to pull out league info for fixtures input

--- api_football.py
+++ api_football.py
@@ -16,14 +16,14 @@ def leagues():
     headers = { 'X-Auth-Token': '7d8ec6405f2f43bca384af37191f7eba', 'X-Response-Control': 'minified' }
     connection.request('GET', '/v1/competitions/', None, headers )
     #response returns a list of dictionaries
-    response = json.loads(connection.getresponse().read().decode())
+    response = json.loads(connection.getresponse().read())
     return response
 
 def fixtures(league_id):
     connection = httplib.HTTPConnection('api.football-data.org')
     headers = { 'X-Auth-Token': '7d8ec6405f2f43bca384af37191f7eba', 'X-Response-Control': 'minified' }
-    connection.request('GET', '/v1/competitions/'+league_id+'/fixtures/', None, headers )
-    response = json.loads(connection.getresponse().read().decode())
+    connection.request('GET', '/v1/competitions/'+str(league_id)+'/fixtures/', None, headers )
+    response = json.loads(connection.getresponse().read())
     return response
 
 lgdict = {}
@@ -39,12 +39,19 @@ for i in leagues():
 #print lg[0]
 
 for i in lg:
-    #print i  ## returns each list
-    if i[3] =='Premier League 2016/17':
-        lgid = str(i[0])
-        break
+#    print i  ## returns each list
+    if unicode(i[3]) == 'Premier League 2017/18':
+       lg_id = i[0]
+       
     
-print fixtures(lgid)['fixtures'][380]
+#print lg_id
+    
+#print '/v1/competitions/'+str(lg_id)+'/fixtures/'
+fixture_redux = []
+dict1 = {}
+for i in fixtures(lg_id)['fixtures']:
+    fixture_redux.append([i['homeTeamName'], i['matchday'], i['awayTeamName'], i['date'], i['result']])
+print fixture_redux
     
         
 
